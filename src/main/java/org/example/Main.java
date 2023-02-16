770824bb7ddb204963820571980de11a8b06c4ae
saonguyen2603 <saonguyen260396@gmail.com>
2023-02-16 17:54:19 +0700

    them file DemoGit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ko có gì");
+        System.out.println("Hello world!");
+    }
+}
