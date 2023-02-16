cb5f0f5459fa8a07359031e8b021fb22387e1467
saonguyen2603 <saonguyen260396@gmail.com>
2023-02-16 17:58:26 +0700

    feat: add nhanh1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("nhanh1");
         System.out.println("ko có gì");
         System.out.println("Hello world!");
     }
