b7475de436a650053f2a3a2725dc75db0184c860
saonguyen2603 <saonguyen260396@gmail.com>
2023-02-17 14:22:55 +0700

    them//123313

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("nhanh2");
-        System.out.println("ko có gì");
-        System.out.println("Hello world!");
+    public static void main(String[] args) {//213123123123
+
     }
 }
