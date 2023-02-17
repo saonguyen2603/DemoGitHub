7bce04edf258c22b0b47b10a7b379c6a91d4eec2
saonguyen2603 <saonguyen260396@gmail.com>
2023-02-17 14:33:24 +0700

    fix: fix conflict

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,7 @@ package org.example;
 import java.sql.SQLOutput;
 
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-=======
     public static void main(String[] args) {//213123123123
 
->>>>>>> nhanh2
     }
 }
